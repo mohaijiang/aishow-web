e515637744876d4beb73fc3ba36523b1c5fe7ef0
leary423 <leary620@163.com>
2023-06-21 10:47:32 +0800

    add tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,7 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: "class",
-  content: ["./index.html", "./App.vue", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
