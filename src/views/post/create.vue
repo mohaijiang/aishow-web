7b8f40b4025e4895f9d07b89cd9484407ad54d00
Dennisic\Chen <cgyforwork@foxmail.com>
2023-06-28 16:53:13 +0800

    feat: save code

--- src/views/post/create.vue
+++ src/views/post/create.vue
@@ -50,6 +50,10 @@ import type { UploadChangeParam } from 'ant-design-vue';
 import Wangeditor from '@/components/Wangeditor.vue';
 import { useRouter } from 'vue-router';
 import { v4 as uuidv4 } from 'uuid';
+import {CreateModelVO, PolkadotAiChanClient} from "@/components/polkadot/ai-model"
+import {ApiPromise, WsProvider} from "@polkadot/api";
+import {web3Accounts, web3Enable} from "@polkadot/extension-dapp";
+import { uploadFileToCloud } from '@/utils/deoss'
 const router = useRouter()
 
 const fileList = ref([]);
@@ -69,9 +73,31 @@ const cancelPost = ()=>{
   router.push('/')
 }
 const handleSubmit = async () => {
-  // await formRef.value.validate();
+  await formRef.value.validate();
   let uuid = uuidv4()
   console.log('uuid',uuid)
+  const allInjected = await web3Enable('my cool dapp');
+  console.log(allInjected)
+  const allAccounts = await web3Accounts();
+  const account = allAccounts[0].address
+  const wsProvider = new WsProvider('wss://ws.aishow.hamsternet.io');
+  const api = await ApiPromise.create({provider: wsProvider});
+  const client = new PolkadotAiChanClient(api,account)
+  const createPostParams = {
+    // 模型hash
+    modelHash: '',
+    // uuid
+    uuid: uuid,
+    // 主题名
+    name: '',
+    // 图片列表
+    images: [],
+    // markdown 备注
+    comment: '',
+  }
+  client.createPost(createPostParams,(status) => {
+    console.log(status)
+  })
   router.push('/postDetail')
 }
 const handleChange = (info: UploadChangeParam) => { 
